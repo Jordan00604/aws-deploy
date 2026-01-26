d37784cf8b5aae6c0d9d892693312a332930eecd
Jordan00604 <jordyguay04@gmail.com>
2026-01-26 16:33:53 -0500

    Primer commit Spring Boot aws-deploy

---
+++ src/main/java/com/awsdeploy/controller/AWSControllerTest.java
@@ -0,0 +1,24 @@
+package com.awsdeploy.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/aws/api")
+public class AWSControllerTest {
+
+    @GetMapping("/hello")
+    public String helo(){
+            return "hello AWS - GET!!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(){
+        return "hello AWS - POST !!";
+    }
+
+}
